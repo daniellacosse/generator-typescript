0e4565002d1a70779dcb83790255ff7ed94335e8
Daniel LaCosse <d@niellacos.se>
2019-03-30 12:02:32 -0400

    seems to work

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,88 +3,77 @@ const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
 
-var fs = require("fs");
-var path = require("path");
-
-function forEachFile(directoryName, callback) {
-  fs.readdir(directoryName, function(e, files) {
-    if (e) {
-      throw new Error(e);
-    }
-
-    files.forEach(function(file) {
-      var fullPath = path.join(directoryName, file);
-
-      fs.stat(fullPath, function(e, f) {
-        if (e) {
-          throw new Error(e);
-        }
-
-        if (f.isDirectory()) {
-          forEachFile(fullPath);
-        } else {
-          callback(fullPath);
-        }
-      });
-    });
-  });
-};
+const exec = require("child_process").exec;
 
 module.exports = class extends Generator {
-  copyTemplate = filename => {
-    this.fs.copyTpl(
-      this.templatePath(path.join(this.sourceRoot(), filename)),
-      this.destinationPath(path.join(this.destinationRoot(), filename)),
-      this.props
-    );
-  };
-
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the my ${chalk.red("generator-typescript")} generator!`)
     );
 
-    const prompts = [
-      {
-        type: "input",
-        name: "displayTitle",
-        message: "your app's name"
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "describe your app"
-      },
-      {
-        type: "input",
-        name: "username",
-        message: "your github username"
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "your github email"
-      },
-      {
-        type: "input",
-        name: "gcpProjectID",
-        message: "your project ID in GCP"
-      }
-    ];
+    return new Promise((resolve, reject) => {
+      exec("git config 'user.email'", (error, stdout) => {
+        if (error) {
+          return reject(error);
+        }
 
-    return this.prompt(prompts).then(props => {
-      props.title = props.displayTitle.toLowerCase().replace(/\s+/g, "-");
+        return resolve(stdout);
+      });
+    })
+      .then(email => {
+        const prompts = [
+          {
+            type: "input",
+            name: "displayTitle",
+            message: "your app's name",
+            default: this.appname
+          },
+          {
+            type: "input",
+            name: "description",
+            message: "describe your app",
+            default: `An app called '${this.appname}'`
+          },
+          {
+            type: "input",
+            name: "username",
+            message: "your github username"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "your email address",
+            default: email.trim()
+          },
+          {
+            type: "input",
+            name: "gcpProjectID",
+            message: "your project ID in GCP",
+            default: "todo-123"
+          }
+        ];
 
-      this.props = props;
-    });
+        return this.prompt(prompts);
+      })
+      .then(props => {
+        props.title = props.displayTitle.toLowerCase().replace(/\s+/g, "-");
+
+        this.props = props;
+      });
   }
 
   writing() {
-    forEachFile(this.sourceRoot(), this.copyTemplate);
+    this.fs.copyTpl(
+      this.templatePath(),
+      this.destinationPath(),
+      this.props,
+      {},
+      { globOptions: { dot: true } }
+    );
   }
 
   install() {
-    this.installDependencies();
+    this.yarnInstall();
   }
 };
